b4660844e366fee32482d7563f9eb4b93bbfdd60
faolin <Movefort4>
2021-05-12 11:18:34 +0200

    ajout de la récupération du clip

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,12 +1,9 @@
-"""module permettant de créer à partir d'un texte un fichier audio """
+"""module permettant de créer à partir d'un texte un fichier audio (text to speech) """
 import requests
 import argparse
+import logging
+import json
 
-
-def get_all_voices():
-  """Permet de récupérer la liste de toutes les voix"""
-  r = requests.get("https://app.resemble.ai/api/v1/voices", headers={'Authorization': token})
-  return r.json()
 
 def create_new_clip(titre, texte, voix):
   """
@@ -14,7 +11,7 @@ def create_new_clip(titre, texte, voix):
   :param titre: le titre du clip
   :param texte: le texte que l'on souhaite convertir en audio
   :param voix: la voix qu'on souhaite utiliser
-  :return: status code de l'API au format json
+  :return clip_id: l'id du clip audio créer
   """
   data = {
     "data": {
@@ -22,13 +19,49 @@ def create_new_clip(titre, texte, voix):
       "body": texte,
       "voice": voix
     },
-    "callback_uri": "https://mycall.back/service"
+    "callback_uri": "https://webhook.site/c4e20a8a-217f-4d9e-b6a2-de4094c99dfa"
   }
   r = requests.post(f"https://app.resemble.ai/api/v1/projects/{project}/clips",
                     headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {bearer}'},
-                    data=data)
-  print(r.json)
-  return r.json
+                    json=data)
+  try:
+    clip_id = r.json()['id']
+    logging.info(f"clip créer sous l'id: {clip_id} dans le projet avec l'id: {r.json()['project_id']}")
+    return clip_id
+  except json.decoder.JSONDecodeError:
+    logging.error(f"Erreur lors de l'éxécution de la requête POST, code retour: {r.status_code}")
+    exit(1)
+
+
+def get_url_clip(clip_id, project_uuid):
+  """
+  méthode permettant de récupérer le lien du fichier du clip que l'on souhaite télécharger
+  :param clip_id: l'id du clip
+  :param project_uuid: l'uuid du projet
+  :return link: l'url du fichier du clip
+  """
+  r = requests.get(f"https://app.resemble.ai/api/v1/projects/{project_uuid}/clips/{clip_id}",
+                   headers={'Authorization': token})
+  print(r.json())
+  try:
+    link = r.json()['link']
+    logging.info(f"Lien du clip récupérer: {link}")
+    return link
+  except json.decoder.JSONDecodeError:
+    logging.error(f"Erreur lors de la récupération de l'url du clip', code retour: {r.status_code}")
+    exit(1)
+
+
+def download_clip(clip_link, titre, output_path):
+  r = requests.get(clip_link)
+  if r.status_code == 200:
+    with open(f"{output_path}/{titre}.mp3", 'wb') as f:
+      f.write(r.content)
+      logging.info(f"clip enregistré sous: {output_path}/{titre}.mp3")
+  else:
+    logging.error("Erreur lors du téléchargement du clip, vérifiez que l'argument output_path soit un chemin valide")
+    exit(1)
+
 
 if __name__ == '__main__':
   # gestion des arguments
@@ -39,6 +72,7 @@ if __name__ == '__main__':
   parser.add_argument('-p', '--project_name', action="store", help="nom du projet de l'API", required=True)
   parser.add_argument('-b', '--bearer', action="store", help="Bearer de l'API pour l'authentification", required=True)
   parser.add_argument('-v', '--voice_uuid', action="store", help="l'UUID de la voice qu'on souhaite utiliser", required=True)
+  parser.add_argument('-o', '--output_path', action="store", help= "Path du répértoire de sortie du fichier", required=True)
   args = parser.parse_args()
 
   # création des variables d'authentification
@@ -47,7 +81,13 @@ if __name__ == '__main__':
   token = args.token
 
   # création du nouveau clip
-  create_new_clip(args.titre, args.texte, args.voice_uuid)
+  clip_id = create_new_clip(args.titre, args.texte, args.voice_uuid)
+
+  # récupération de l'url du clip
+  clip_link = get_url_clip(clip_id, args.project_name)
+
+  # téléchargement du fichier
+  download_clip(clip_link, args.titre, args.output_path)
 
 
 
